32a23a4369d983de26011c2c5521efabe1c33e98
Salieri <shimahfy@gmail.com>
2021-03-15 02:57:27 +0800

    bug fixed and add local proxy

--- src/main/java/com/salieri/community/controller/AuthorizeController.java
+++ src/main/java/com/salieri/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package com.salieri.community.controller;
 
 import com.salieri.community.dto.AccessTokenDTO;
+import com.salieri.community.dto.GithubUser;
 import com.salieri.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,17 +14,19 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
-
-    @GetMapping("callback")
+    @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8787/callback");
+        accessTokenDTO.setState(state);
         accessTokenDTO.setClient_id("6ae5d1dc4818592a298d");
         accessTokenDTO.setClient_secret("5498c97394240f288e17b089b1ee32ef6c65c1e1");
-        accessTokenDTO.setRedirect_uri("http://localhost:8787/callback");
-        accessTokenDTO.setCode(code);
-        accessTokenDTO.setState(state);
-        githubProvider.getAccessToken(accessTokenDTO);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        System.out.println("accessToken= " + accessToken);
+        GithubUser user = githubProvider.getUser(accessToken/*"9880e0b5bb634dc57c19684aa4f4225ca9ba0042"*/);
+        System.out.println("username: " + user.getName());
         return "index";
     }
 
--- src/main/java/com/salieri/community/provider/GithubProvider.java
+++ src/main/java/com/salieri/community/provider/GithubProvider.java
@@ -1,6 +1,8 @@
 package com.salieri.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.salieri.community.dto.AccessTokenDTO;
+import com.salieri.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -9,25 +11,55 @@ import java.io.IOException;
 @Component
 public class GithubProvider {
 
+
+    // 获得AccessToken
     public String getAccessToken(AccessTokenDTO accessTokenDTO) {
 
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        System.setProperty("https.proxySet", "true");
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "7890");
+
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-            RequestBody body = RequestBody.create(JSON, json);
+            RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
             Request request = new Request.Builder()
                     .url("https://github.com/login/oauth/access_token")
                     .post(body)
                     .build();
             try (Response response = client.newCall(request).execute()) {
                 String string = response.body().string();
-                System.out.println(string);
-                return string;
-    } catch (IOException e) {
+                String token = string.split("&")[0].split("=")[1];
+                return token;
+    } catch (Exception e) {
+                System.out.println("response failed");
                 e.printStackTrace();
             }
             return null;
+    }
+
+    // 根据AccessToken获取user信息
+    public GithubUser getUser(String accessToken) {
+
+        System.setProperty("https.proxySet", "true");
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "7890");
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .header("Authorization","token " + accessToken)
+                .build();
+        try {
+            Response response = okHttpClient.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class); //json转换成类
+            return githubUser;
+        } catch (IOException e) {
+
+        }
+        return null;
     }
 
 }
