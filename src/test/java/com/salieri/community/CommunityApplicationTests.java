46fa0571dbfdd6e8a3652ac36477ba1e1666e628
Salieri <shimahfy@gmail.com>
2021-03-14 18:57:28 +0800

    init repository

---
+++ src/test/java/com/salieri/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salieri.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
