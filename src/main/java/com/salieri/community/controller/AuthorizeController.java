a7e63d10326610dc4940c20ac7f20f871b1082f7
Salieri <shimahfy@gmail.com>
2021-03-16 17:32:35 +0800

    实现持久化登录

--- src/main/java/com/salieri/community/controller/AuthorizeController.java
+++ src/main/java/com/salieri/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirect_uri);
@@ -45,14 +48,24 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser != null){
-            // 登录成功, 写 session和cookie
+            // 登录成功
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            // 生成token
+            String token = UUID.randomUUID().toString();
+            // 信息放入User对象中
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            // 存入数据库中
             userMapper.insert(user);
+            // token放入cookie中
+            response.addCookie(new Cookie("token",token));
+            // 写 session和cookie
+
+
+
             request.getSession().setAttribute("githubUser",githubUser); //user对象放入session中
             return "redirect:/";
         } else {
--- src/main/java/com/salieri/community/mapper/UserMapper.java
+++ src/main/java/com/salieri/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.salieri.community.mapper;
 import com.salieri.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface UserMapper {
     @Insert("INSERT INTO USER (name,account_id,token,gmt_create,gmt_modified) VALUES (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token} ")
+    User findByToken(@Param("token") String token);
 }
