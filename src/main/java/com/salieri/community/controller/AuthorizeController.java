2be76f40634f622acb8af4bae7861304b4e62a62
Salieri <shimahfy@gmail.com>
2021-03-16 16:21:55 +0800

    添加数据库连接

--- src/main/java/com/salieri/community/controller/AuthorizeController.java
+++ src/main/java/com/salieri/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.salieri.community.controller;
 
 import com.salieri.community.dto.AccessTokenDTO;
 import com.salieri.community.dto.GithubUser;
+import com.salieri.community.mapper.UserMapper;
+import com.salieri.community.model.User;
 import com.salieri.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,6 +29,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirect_uri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -37,10 +43,17 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
             // 登录成功, 写 session和cookie
-            request.getSession().setAttribute("user",user); //user对象放入session中
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("githubUser",githubUser); //user对象放入session中
             return "redirect:/";
         } else {
             // 登录失败, 重新登录
---
+++ src/main/java/com/salieri/community/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.salieri.community.mapper;
+
+import com.salieri.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UserMapper {
+    @Insert("INSERT INTO USER (name,account_id,token,gmt_create,gmt_modified) VALUES (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
